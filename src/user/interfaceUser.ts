4ef72cd8cee1c83ab2e37d5ffa30257ee9f99807
elieldiniz <elieldiniz1@outlook.com>
2024-08-26 21:24:19 -0400

    atualização de dependencias de projetos expres - > fastify

--- src/user/interfaceUser.ts
+++
@@ -1,9 +0,0 @@
-import UserError from "./userError"
-
-export interface InterfaceUser {
-    name: string
-    email: string
-    password: string
-
-    Created(): string | UserError
-}
--- src/user/userError.ts
+++
@@ -1,6 +0,0 @@
-export default class UserError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = 'UserError';
-    }
-}
